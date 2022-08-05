82dc232c2cd5bc78a06a5a7246b407d14df1d06f
ryogavege <r.furuya@4nonome.com>
2022-08-06 04:11:45 +0900

    topic4完了

---
+++ AOJ/04/04_A.py
@@ -0,0 +1,19 @@
+def d(m, n):
+    d = m // n
+    return d
+    
+def r(x, y):
+    r = x % y
+    return r
+ 
+def f(k, l):
+    f = round(k / l, 5)
+    return f
+
+a, b = map(int, input().split())
+
+d = d(a, b)
+r = r(a, b)
+f = f(a, b)
+
+print(d, r, f)
---
+++ AOJ/04/04_B.py
@@ -0,0 +1,14 @@
+import math
+
+def area(x):
+    a = round(math.pi*x**2, 6)
+    return a
+    
+def circ(y):
+    c = round(2*y*math.pi, 6)
+    return c
+    
+r = round(float(input()), 6)
+Area = area(r)
+Circ = circ(r)
+print(Area, Circ)
