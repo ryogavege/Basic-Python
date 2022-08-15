65e3191f41cd91af99fc6aab28814440076de888
ryogavege <r.furuya@4nonome.com>
2022-08-15 19:20:17 +0900

    topic7をコミット

--- AOJ/07/07_B.py
+++ AOJ/07/07_B.py
@@ -1,7 +1,11 @@
-n, x = map(int, input().split())
-
-1 <= a < b < c <= n
-a + b + c = x
-
-if n = 0:
-    break
+while True:
+    n, x = map(int, input().split())
+    if n == 0:
+        break
+    count = 0
+    for i in range(1, n - 1):
+        for k in range(i + 1, n):
+            for m in range(k + 1, n + 1):
+                if i + k + m == x:
+                    count += 1
+    print(count)
